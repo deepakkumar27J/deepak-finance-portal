f5c8e6e44a241ceaa222a77eda6983491e9cf268
deepakkumar27J <90575935+deepakkumar27J@users.noreply.github.com>
2023-05-07 16:17:23 +0100

    unit tests for invocie

--- src/invoice/invoice.controller.spec.ts
+++ src/invoice/invoice.controller.spec.ts
@@ -1,14 +1,26 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { InvoiceController } from './invoice.controller';
-import { InvoiceService } from './invoice.service';
+import { InvoiceService, InvoiceType } from './invoice.service';
+import { AccountModule } from 'src/account/account.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Invoice } from './entities/invoice.entity';
 
 describe('InvoiceController', () => {
   let controller: InvoiceController;
+  let createInvoiceDto = {
+    "amount":5,
+    "dueDate":new Date(),
+    "types":InvoiceType.Course,
+    "studentEmail":"C@deepak.com"
+
+  }
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [InvoiceController],
       providers: [InvoiceService],
+      imports:[TypeOrmModule.forFeature([Invoice]), AccountModule],
+      exports:[InvoiceService]
     }).compile();
 
     controller = module.get<InvoiceController>(InvoiceController);
@@ -17,4 +29,10 @@ describe('InvoiceController', () => {
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
+
+  describe('createInvoice', ()=>{
+    it('should return 200', ()=>{
+      controller.create(createInvoiceDto);
+    })
+  })
 });
